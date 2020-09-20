4d33edadb1eb7864f63517163d29eedb995ca2ca
giang <nguyengiang.nchg@gmail.com>
2020-09-20 22:36:01 +0300

    Initial commit

---
+++ assignment.js
@@ -0,0 +1,83 @@
+/**  
+Orientation - JS assignment 1
+Solution by: [Giang Nguyen]
+*/
+
+let flipReuslts, userGuess, points
+const coin = ['head', 'reverse']
+
+init()
+
+function checkUserChoice() {
+  const userChoice = document.querySelector('input[name="user-choice"]:checked').value;
+  console.log('The user choice is', userChoice);
+  return userChoice;
+}
+
+function displayWins() {
+  const wins = document.querySelector('#win-count');
+  wins.textContent = points
+  return wins
+}
+
+function displayResultHistory() {
+  const results = document.querySelector('#result-history');
+  results.textContent = userGuess
+  return results
+}
+
+function displayHead() {
+  const head = document.querySelector('#head-count');
+  head.textContent = flipReuslts.filter(a => a === 'head').length
+  return head
+}
+
+function displayReverse() {
+  const reverse = document.querySelector('#reverse-count');
+  reverse.textContent = flipReuslts.filter(a => a === 'reverse').length
+  return reverse
+}
+
+// Function to allow users to throw coins
+function throwCoin() {
+  // 1. Record user's choice
+  const userChoice = checkUserChoice()
+
+  // 2. Toss coin
+  const result = Math.round(Math.random())
+  flipReuslts.push(coin[result])
+  console.log('The result of throwing coin is', coin[result])
+ 
+  // 2. Show the result
+  const coinImage = document.querySelector('#coin-image');
+  coinImage.style.display = 'block'
+  coinImage.src = `${coin[result]}`+ '.png'
+
+  const resultParagraph = document.querySelector('#result');
+  if(userChoice === coin[result]) {
+    resultParagraph.textContent = 'Your guess was correct!'
+    userGuess.push('Correct')
+  } else {
+    resultParagraph.textContent = 'Your guess was wrong!' 
+    userGuess.push('Wrong')
+  }
+
+  // 3. Modify the points
+  userChoice === coin[result] ? points += 1 : points
+
+  // 4. Display statistics
+  displayWins()
+  displayResultHistory()
+  displayHead()
+  displayReverse()
+}
+
+const throwButton = document.querySelector('#throw-button');
+throwButton.addEventListener('click', throwCoin);
+
+function init() {
+  flipReuslts = [] 
+  userGuess = []
+  points = 0
+  document.querySelector('#coin-image').style.display = 'none';
+}
